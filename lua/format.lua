0c82cd6d4bea6aa3b0ec8a71513bef98ecc3c647
licong <licong@qianxin.com>
2022-01-21 13:01:20 +0800

    支持 json format

--- lua/format.lua
+++ lua/format.lua
@@ -1,16 +1,17 @@
-require('formatter').setup({
-  filetype = {
-    javascript = {
-      -- prettier
-      function()
-        return {
-          exe = "prettier",
-          args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), '--single-quote'},
-          stdin = true
-        }
-      end
-    },
-    lua = {
+require("formatter").setup(
+  {
+    filetype = {
+      javascript = {
+        -- prettier
+        function()
+          return {
+            exe = "prettier",
+            args = {"--stdin-filepath", vim.fn.fnameescape(vim.api.nvim_buf_get_name(0)), "--single-quote"},
+            stdin = true
+          }
+        end
+      },
+      lua = {
         -- luafmt
         -- sudo npm install -g lua-fmt
         function()
@@ -20,44 +21,53 @@ require('formatter').setup({
             stdin = true
           }
         end
-    },
-    python = {
-      -- Configuration for psf/black
-      function()
-        return {
-          exe = "black", -- this should be available on your $PATH
-          args = { '-' },
-          stdin = true,
-        }
-      end
-    },
-    go = {
+      },
+      python = {
+        -- Configuration for psf/black
         function()
-            return {
-                exe = "goimports",
-                args = { '--' },
-                stdin = true,
-            }
+          return {
+            exe = "black", -- this should be available on your $PATH
+            args = {"-"},
+            stdin = true
+          }
         end
-    },
-    ocaml = {
+      },
+      go = {
         function()
-            return {
-                exe = "ocamlformat",
-                args = {"--name tmp.ml --enable-outside-detected-project -"},
-                stdin = true,
-            }
+          return {
+            exe = "goimports",
+            args = {"--"},
+            stdin = true
+          }
         end
-    },
-    perl = {
+      },
+      ocaml = {
         function()
-            return {
-                exe = "perltidy",
-                args = {"-"},
-                stdin = true,
-            }
+          return {
+            exe = "ocamlformat",
+            args = {"--name tmp.ml --enable-outside-detected-project -"},
+            stdin = true
+          }
         end
+      },
+      perl = {
+        function()
+          return {
+            exe = "perltidy",
+            args = {"-"},
+            stdin = true
+          }
+        end
+      },
+      json = {
+        function()
+          return {
+            exe = "python",
+            args = {"-m json.tool"},
+            stdin = true
+          }
+        end
+      }
     }
-  }
   }
 )
